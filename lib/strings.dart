d7249fdecfdef8c5db6ee375e4c8d783626a760c
mariaelizabeth198 <silvamay198@gmail.com>
2021-08-23 15:06:49 -0300

    Add strings file

---
+++ lib/strings.dart
@@ -0,0 +1,8 @@
+
+class Strings {
+  static const fieldEmailTitle = "E-mail";
+  static const fieldPasswordTitle = "Senha";
+  static const loginButtonText = "Entrar";
+  static const signupButtonText = "Cadastrar";
+  static const forgotPasswordButtonText = "Esqueci a senha";
+}
