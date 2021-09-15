8b3f7eb02249394c3736f7895cc6b0b5225023b2
mariaelizabeth198 <silvamay198@gmail.com>
2021-09-15 20:15:54 -0300

    add v0 of product dialog

--- lib/ui/home/productDialog.dart
+++ lib/ui/home/productDialog.dart
@@ -29,45 +29,54 @@ class _FullScreenLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(color: CustomColors.backgroundGrey),
-        child: Center(
-            child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.close,
-                    color: CustomColors.primaryColor,
-                    size: CustomDimens.closeButtonSize,
+      decoration: BoxDecoration(color: CustomColors.backgroundGrey),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(CustomDimens.smallSpacing,
+              0.0, CustomDimens.smallSpacing, 0.0),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.close,
+                      color: CustomColors.primaryColor,
+                      size: CustomDimens.closeButtonSize,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
                 ),
-              ),
-              Text("TEsteeeee alalalala lala",
-                  style: TextStyle(
-                    fontFamily: 'TTNorms',
-                    fontWeight: FontWeight.bold,
-                    wordSpacing: 0,
-                    letterSpacing: 0,
-                    fontSize: 25,
-                    color: Colors.yellow,
-                  )),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(CustomDimens.smallSpacing,
-                    CustomDimens.smallSpacing, CustomDimens.smallSpacing, 0.0),
-                child: Image.asset(
-                  'assets/images/logo_aluguei.png',
-                  height: CustomDimens.logoSize,
-                  fit: BoxFit.cover,
+                Text("TEsteeeee alalalala lala",
+                    style: TextStyle(
+                      fontFamily: 'TTNorms',
+                      fontWeight: FontWeight.bold,
+                      wordSpacing: 0,
+                      letterSpacing: 0,
+                      fontSize: 25,
+                      color: Colors.yellow,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                      CustomDimens.smallSpacing,
+                      CustomDimens.smallSpacing,
+                      CustomDimens.smallSpacing,
+                      0.0),
+                  child: Image.asset(
+                    'assets/images/logo_aluguei.png',
+                    height: CustomDimens.logoSize,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
