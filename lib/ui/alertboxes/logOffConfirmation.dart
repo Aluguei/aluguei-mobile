86197f3b22dd5c4c4976ffbe1437489e2b6d8b99
mariaelizabeth198 <silvamay198@gmail.com>
2021-11-17 15:09:12 -0300

    logout

--- lib/ui/alertboxes/logOffConfirmation.dart
+++ lib/ui/alertboxes/logOffConfirmation.dart
@@ -1,28 +1,37 @@
+import 'package:aluguei/repository/models/authentication/loginResponse.dart';
 import 'package:aluguei/ui/home/home.dart';
+import 'package:aluguei/ui/loginScreen/login.dart';
 import 'package:flutter/material.dart';
 import 'package:aluguei/resources/strings.dart';
+import 'package:hive/hive.dart';
 
 showAlertDialog(BuildContext context) {
+  Future<void> cleanCache() async {
+    Hive.box('loginResponse').clear();
+  }
 
+  void logoutUser() {
+    cleanCache();
 
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(
+        builder: (BuildContext context) => LoginPage(title: "login"),
+      ),
+      (Route route) => false,
+    );
+  }
 
   Widget cancelButton = FlatButton(
     child: Text(Strings.AlertNoConfirmation),
     onPressed: () {
       Navigator.of(context).pop();
-
-
-
-
-
-
-
     },
   );
+
   Widget continueButton = FlatButton(
     child: Text(Strings.AlertYesConfirmation),
     onPressed: () {
-      //TODO COLOCAR AQUI O LOGOUT
+      logoutUser();
     },
   );
 
@@ -40,6 +49,4 @@ showAlertDialog(BuildContext context) {
       return alert;
     },
   );
-
-
 }
